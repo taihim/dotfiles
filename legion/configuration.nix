2e3c59f517db970f7f5fd1b389fb4ffb7b4b9576
Taimur Ibrahim <tai.him18@gmail.com>
2026-06-01 16:35:21 +0200

    Update configuration.nix

--- legion/configuration.nix
+++ legion/configuration.nix
@@ -60,14 +60,10 @@ in
 
   # Battery health & CPU frequency scaling
   services.tlp = {
-    enable = true;
+    enable = false;
     settings = {
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 80;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
     };
   };
 
